b0e7249171e728fbb10a26d681e67c4f60421fa6
Dmitry <dimas2568.shipilo@mail.ru>
2025-01-16 19:34:58 +0300

    Create determine func

---
+++ src/determine_func.py
@@ -0,0 +1,17 @@
+
+
+def get_score(game_stamps, offset):
+    '''
+        Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
+        Please pay attention to that for some offsets the game_stamps list may not contain scores.
+    '''
+    try:
+        offset = int(offset)
+    except (TypeError, ValueError) as exc:
+        if isinstance(exc, ValueError):
+            return f"Invalid literal for int() with base 10: <{offset}>."
+        return "Incorrect offset. Offset must be <str>, <float>, <byte> or <int>."
+    for stamp in game_stamps:
+        if int(offset) == stamp["offset"]:
+            return stamp["score"]["home"], stamp["score"]["away"]
+    return "Offset not found."
